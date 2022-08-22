fed5e60e6d6fc5353009b848993a24a048f15144
Michael Arnaldi <arnaldimichael@gmail.com>
2022-08-22 12:09:42 +0000

    chore: add manage integration for acquire stage

--- packages/otel/src/Span/index.ts
+++ packages/otel/src/Span/index.ts
@@ -1,7 +1,10 @@
 // ets_tracing: off
 
+import * as Tp from "@effect-ts/core/Collections/Immutable/Tuple"
 import * as T from "@effect-ts/core/Effect"
 import { pretty } from "@effect-ts/core/Effect/Cause"
+import * as M from "@effect-ts/core/Effect/Managed"
+import type * as RM from "@effect-ts/core/Effect/Managed/ReleaseMap"
 import { pipe } from "@effect-ts/core/Function"
 import type { Has } from "@effect-ts/core/Has"
 import { tag } from "@effect-ts/core/Has"
@@ -9,8 +12,7 @@ import * as O from "@effect-ts/core/Option"
 import * as OTApi from "@opentelemetry/api"
 import { context, trace } from "@opentelemetry/api"
 
-import type { Tracer } from "../Tracer/index.js"
-import { withTracer } from "../Tracer/index.js"
+import { Tracer, withTracer } from "../Tracer/index.js"
 
 export const SpanSymbol = Symbol()
 export type SpanSymbol = typeof SpanSymbol
@@ -23,6 +25,51 @@ export class SpanImpl {
 export interface Span extends SpanImpl {}
 
 export const Span = tag<Span>()
+
+export function withAcquireSPan(
+  name: string,
+  options?: OTApi.SpanOptions,
+  ctx?: OTApi.Context
+) {
+  return <R, E, A>(
+    managed: M.Managed<R & Has<Span>, E, A>
+  ): M.Managed<R & Has<Tracer>, E, A> => {
+    return new M.ManagedImpl(
+      T.bracketExit_(
+        T.access(({ tuple: [r] }: Tp.Tuple<[R & Has<Tracer>, RM.ReleaseMap]>) => {
+          const { tracer } = Tracer.read(r)
+          const maybeSpan = Span.readOption(r)
+          if (ctx) {
+            return tracer.startSpan(name, options, ctx)
+          }
+          if (options?.root !== true && O.isSome(maybeSpan)) {
+            const ctx = trace.setSpan(context.active(), maybeSpan.value.span)
+            return tracer.startSpan(name, options, ctx)
+          }
+          return tracer.startSpan(name, { ...options, root: true })
+        }),
+        (span) =>
+          T.provideSome_(
+            managed.effect,
+            ({ tuple: [rest, rm] }: Tp.Tuple<[R & Has<Tracer>, RM.ReleaseMap]>) =>
+              Tp.tuple({ ...Span.has(new SpanImpl(span)), ...rest }, rm)
+          ),
+        (s, e) =>
+          T.succeedWith(() => {
+            if (e._tag === "Failure") {
+              s.setAttribute("error.type", "Fiber Failure")
+              s.setAttribute("error.message", "An Effect Has A Failure")
+              s.setAttribute("error.stack", pretty(e.cause))
+              s.setStatus({ code: OTApi.SpanStatusCode.ERROR })
+            } else {
+              s.setStatus({ code: OTApi.SpanStatusCode.OK })
+            }
+            s.end()
+          })
+      )
+    )
+  }
+}
 
 export function withSpan(
   name: string,
